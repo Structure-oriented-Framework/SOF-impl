bbb462d74abc30f68d76179076b8d1570a6fbc87
_Kerman <kermanx@qq.com>
2023-01-26 03:50:47 +0000

    fix wrong merge

--- src/JsonTunnel.ts
+++ src/JsonTunnel.ts
@@ -1,10 +1,6 @@
 import { Param } from "./port.js";
 import {
-<<<<<<< HEAD
   JSONString,
-=======
-  StringifiedSerializableValues,
->>>>>>> d43d33fd187901a5de94a3560d73e09274234a0f
   safeJSONParse,
   safeJSONStringify,
 } from "./serializableType.js";
